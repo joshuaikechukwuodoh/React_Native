c2fe295e86892403def6326663e6479ad2611450
joshuaikechukwuodoh <odohjoshua82@gmail.com>
2025-02-08 16:01:10 -0800

    My_app_project

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome to JoshuaSmith RealEsate</Text>
+      <Link href = "/sign-in"> <Text>Sign In </Text></Link>
+      <Link href = "/explore"> <Text>Explore</Text> </Link>
+      <Link href = "/profile"> <Text>Profile</Text> </Link>
+      <Link href = "/properties/1"> <Text>Property</Text> </Link>
+    </View>
+  );
+}
